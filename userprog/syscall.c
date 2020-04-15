44438057455c9df12b1df81d0243567a364fa688
marinathomas <mannathomas@gmail.com>
2020-04-15 03:33:05 +0000

    initial commit for file create, open and remove

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -1,6 +1,7 @@
 #include "userprog/syscall.h"
 #include <stdio.h>
 #include <syscall-nr.h>
+#include "filesys/filesys.h"
 #include "userprog/pagedir.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
@@ -9,6 +10,9 @@
 #define CODE_PHYS_BASE 0x08048000
 
 static void syscall_handler (struct intr_frame *);
+bool sys_create ( char *file, unsigned initial_size);
+bool sys_remove ( char *file);
+int sys_open ( char *file);
 int sys_write (int fd, void *buffer, unsigned size);
 void sys_exit (int status);
 bool is_valid_memory_access(uint32_t *pd, const void *vaddr );
@@ -31,7 +35,8 @@ static void syscall_handler (struct intr_frame *f UNUSED)
   }
   uint32_t callNo;
   uint32_t *user_esp = f->esp;
-  uint32_t arg1, arg2, arg3;
+  uint32_t fd, size;
+  void* buffer;
   
   callNo = (uint32_t)(*user_esp);
   //printf("call no %d",callNo);
@@ -42,22 +47,73 @@ static void syscall_handler (struct intr_frame *f UNUSED)
     //shut down machine
     shutdown_power_off();
     break;
+    
+  case SYS_CREATE:
+    user_esp++;
+    if(!is_valid_memory_access(t->pagedir, user_esp)){
+      sys_exit(-1);
+      break;
+    }
+    buffer = (void*)(*((int*)user_esp));
+    if( buffer == NULL || !is_valid_memory_access(t->pagedir, (int *)user_esp)){
+      sys_exit(-1);
+      break;
+    }
+    
+    user_esp++;
+    if(!is_valid_memory_access(t->pagedir, user_esp)){
+      sys_exit(-1);
+      break;
+    } 
+    size = (uint32_t)(*user_esp);
+    f->eax =  sys_create ((char *)buffer, size);    
+    break;
 
+  case SYS_OPEN:
+    user_esp++;
+    if(!is_valid_memory_access(t->pagedir, user_esp)){
+      sys_exit(-1);
+      break;
+    }
+    buffer = (void*)(*((int*)user_esp));
+    if( buffer == NULL || !is_valid_memory_access(t->pagedir, (int *)user_esp)){
+      sys_exit(-1);
+      break;
+    }
+    
+    f->eax =  sys_open ((char *)buffer);
+    break;
+
+  case SYS_REMOVE:
+    user_esp++;
+    if(!is_valid_memory_access(t->pagedir, user_esp)){
+      sys_exit(-1);
+      break;
+    }
+    buffer = (void*)(*((int*)user_esp));
+    if( buffer == NULL || !is_valid_memory_access(t->pagedir, (int *)user_esp)){
+      sys_exit(-1);
+      break;
+    }
+    
+    f->eax =  sys_remove ((char *)buffer);
+    break;
+   
   case SYS_WRITE: //called to output to a file or STDOUT
     user_esp++;
     if(!is_valid_memory_access(t->pagedir, user_esp)){
       sys_exit(-1);
       break;
     }
-    arg1 = (uint32_t)(*user_esp);//fd
+    fd = (uint32_t)(*user_esp);//fd
 
     user_esp++;
     if(!is_valid_memory_access(t->pagedir, user_esp)){
       sys_exit(-1);
       break;
     }
-    //arg2 = (uint32_t)(*user_esp);
-    void* buffer = (void*)(*((int*)user_esp));
+    
+    buffer = (void*)(*((int*)user_esp));
     if(buffer == NULL){
       sys_exit(-1);
       break;
@@ -68,9 +124,9 @@ static void syscall_handler (struct intr_frame *f UNUSED)
       sys_exit(-1);
       break;
     } 
-    arg3 = (uint32_t)(*user_esp);
+    size = (uint32_t)(*user_esp);
         
-    f->eax = sys_write((int)arg1, (char *)buffer, (unsigned )arg3);
+    f->eax = sys_write((int)fd, (char *)buffer, (unsigned )size);
     break;
   case SYS_EXIT:
     user_esp++;
@@ -78,8 +134,8 @@ static void syscall_handler (struct intr_frame *f UNUSED)
 	sys_exit(-1);
 	break;
     }
-    arg1 = (uint32_t)(*user_esp); //exit status
-    sys_exit(arg1);
+    uint32_t status = (uint32_t)(*user_esp); //exit status
+    sys_exit(status);
     break;
   default:
     sys_exit(-1);
@@ -87,13 +143,6 @@ static void syscall_handler (struct intr_frame *f UNUSED)
   
 }
 
-/*Creates a new file called file initially initial_size bytes in size. Returns true if successful, false otherwise.
- Creating a new file does not open it: opening the new file is a separate operation which would require a open system call. */
-bool sys_create (char *file, unsigned initial_size){
-  
-}
-
-  
 
 /*
 checks for validity of the address
@@ -104,6 +153,39 @@ bool is_valid_memory_access(uint32_t *pd, const void *vaddr ){
   } else {
     return false;
   }
+}
+
+
+/*Creates a new file called file initially initial_size bytes in size. Returns true if successful, false otherwise.
+ Creating a new file does not open it: opening the new file is a separate operation which would require a open system call. */
+bool sys_create (char *file, unsigned initial_size){
+  bool status = filesys_create ( file, initial_size);
+  return status;
+}
+
+/*  Deletes the file called file. Returns true if successful, false otherwise. A file may be removed regardless of whether it is open or closed,
+    and removing an open file does not close it. See Removing an Open File, for details.
+ */
+bool sys_remove ( char *file){
+  bool status = filesys_remove ( file);
+  return status;
+}
+
+
+/* Opens the file called file. Returns a nonnegative integer handle called a "file descriptor" (fd), or -1 if the file could not be opened.
+      File descriptors numbered 0 and 1 are reserved for the console: fd 0 (STDIN_FILENO) is standard input, fd 1 (STDOUT_FILENO) is standard output. The open system call will never return either of these file descriptors, which are valid as system call arguments only as explicitly described below.
+
+      Each process has an independent set of file descriptors. File descriptors are not inherited by child processes.
+
+      When a single file is opened more than once, whether by a single process or different processes, each open returns a new file descriptor. Different file descriptors for a single file are closed independently in separate calls to close and they do not share a file position.
+*/
+int sys_open ( char *name){
+  struct file * new_file = filesys_open (name);
+  int fd = -1;
+  if( new_file != NULL){
+    fd = 3;
+  }
+  return fd;
 }
 
 /*
